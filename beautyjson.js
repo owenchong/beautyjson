a417c56df52053663e26f8e5fa9d509279387881
owenchong <zhongxiaogang888@gmail.com>
2012-10-19 15:52:01 +0800

    add some message

--- beautyjson.js
+++ beautyjson.js
@@ -1,3 +1,21 @@
+/**
+  *  BeautyJson is a tool for javascript object format,it is easy to make json readable.
+  *  Usage : 
+  *      beautyJson({id:1,name:'Owen',history:[{date : new Date(),b:true}]}})
+  *    Output :
+  *     { 
+  *    	  "id" : 1, 
+  *    	  "name" : "Owen", 
+  *    	  "history" : [{ 
+  *    	    "date" : "2012-10-19 15:40:41", 
+  *    	    "b" : true
+  *    	  }]
+  *    	}
+  *    
+  *
+  * @author Owen Zhong
+  */
+
 beautyJson = (function(){
 	var space = '    ';
 	function addSpace(buffer,level){
